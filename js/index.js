ea929797502daf3531ce54602130b0fa9e5e7238
Orxan Celilov <orxan.celilov18@gmail.com>
2025-05-13 16:12:17 +0400

    yeni

--- js/index.js
+++ js/index.js
@@ -16,19 +16,19 @@ function renderCards() {
         const card = `
         <div class="bg-white border border-transparent rounded-2xl w-full px-3 py-3 hover:shadow-[0px_4px_20px_0px_rgba(0,0,0,0.2)] transition-all duration-300 relative group">
             <a href="details.html?id=${book.id}" onclick="incrementView(${book.id})">
-                <img class="w-full h-[340px] object-cover rounded-t-lg" src="${book.cover}" alt="" />
+                <img class="w-full h-[340px] object-cover rounded-t-lg mobl" src="${book.cover}" alt="" />
             </a>
             <a href="wishlist.html"
               class="absolute top-5 right-5 text-2xl text-white opacity-0 group-hover:opacity-100 transition-all duration-300">
               <i class="fa-solid fa-heart hover:text-[#ef3340]"></i>
              </a>
-            <div class="p-5 space-y-3">
-                <h5 class="text-[16px] h-auto text-[#0f172a]">
+            <div class="p-5">
+                <h5 class="text-[16px] h-[69px] text-[#0f172a]">
                     <span class="text-[#0f172a] text-[16px] font-bold">${index + 1}.</span> 
                     <a href="details.html?id=${book.id}">
                         ${book.title}</a>
                 </h5>
-                <div class="flex justify-between items-center pt-2 h-[45px]">
+                <div class="flex justify-between items-center h-[45px]">
                     <span class="font-bold uppercase text-[#1e293b] text-[18px]">${book.price}₼</span>
                 </div>
             </div>
